0c16a15bccfa97a189f0dcdfdb133c90e4ae8cba
Justin Levy <justinlevy20@gmail.com>
2022-06-13 11:00:21 -0400

    Footer text size hotfix

--- components/Projects.js
+++ components/Projects.js
@@ -8,31 +8,7 @@ function Projects() {
     <div id='projects'>
       <div className='pGrid'>
 
-        {/* 1. Voxplor */}
-
-        <div className='gridCard'>
-          <div className='thumbnail'>
-            <h1>Voxplor</h1>
-            <Image src='/placeholderThumb.png' layout='fill' />
-          </div>
-          <div className='desc'>
-            <h1>Brief</h1>
-            <p><i>Voxplor is game built to display the features and capabilities of my voxel engine, SimplyVoxel.</i></p>
-          </div>
-          <div className='tools'>
-            <h1>Stack</h1>
-            <div className='items'>
-              <p><i>For updates on the timeline, visit my YouTube</i></p>
-            </div>
-          </div>
-          <div className='links'>
-            <button onClick={() => window.open('', '_blank')}>
-              <FaGlobe size={24}/>
-            </button>
-          </div>
-        </div>
-
-        {/* 2. SimplyVoxel */}
+        {/* 2. SimplyVoxel 
 
         <div className='gridCard'>
           <div className='thumbnail'>
@@ -56,7 +32,7 @@ function Projects() {
           </div>
         </div>
 
-        {/* 3. VisualizeIt */}
+        {/* 3. VisualizeIt 
 
         <div className='gridCard'>
           <div className='thumbnail'>
@@ -83,6 +59,8 @@ function Projects() {
           </div>
         </div>
 
+        */}
+
         {/* 4. Portfolio */}
 
         <div className='gridCard'>
@@ -106,57 +84,6 @@ function Projects() {
             </Link>
             <button onClick={() => window.open('https://github.com/JLevy18/NextNetlifyPortfolio', '_blank')}>
               <FaCode size={24}/>
-            </button>
-          </div>
-        </div>
-
-        {/* 5. MovieApp */}
-
-        <div className='gridCard'>
-          <div className='thumbnail'>
-            <h1>Movie App</h1>
-            <Image src='/placeholderThumb.png' layout='fill' />
-          </div>
-          <div className='desc'>
-            <h1>Brief</h1>
-            <p><i>A webapp tool to search the internet movie database and find information on a particular movie.</i></p>
-          </div>
-          <div className='tools'>
-            <h1>Stack</h1>
-            <div className='items'>
-              <p><i>For updates on the timeline, visit my YouTube</i></p>
-            </div>
-          </div>
-          <div className='links'>
-            <button onClick={() => window.open('', '_blank')}>
-              <FaGlobe size={24}/>
-            </button>
-            <button onClick={() => window.open('', '_blank')}>
-              <FaCode size={24}/>
-            </button>
-          </div>
-        </div>
-
-        {/* 6. RLGL */}
-
-        <div className='gridCard' id='rlgl'>
-          <div className='thumbnail'>
-            <h1>Red Light Green Light</h1>
-            <Image src='/RLGLThumb.png' layout='fill' />
-          </div>
-          <div className='desc'>
-            <h1>Brief</h1>
-            <p>Video game created to aid in the study of a psychological behavior known temporal discounting. In use at Ohio University to perform the case study.</p>
-          </div>
-          <div className='tools'>
-            <h1>Stack</h1>
-            <div className='items'>
-              <p>Unreal</p><p>Git</p><p>Agile/Scrum</p>
-            </div>
-          </div>
-          <div className='links'>
-            <button onClick={() => window.open('', '_blank')}>
-              <FaGlobe size={24}/>
             </button>
           </div>
         </div>
