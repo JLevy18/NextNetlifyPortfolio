bac6bf371b3e69902b599b36557c9573f84cd0ff
Justin Levy <justinlevy20@gmail.com>
2022-06-13 11:17:49 -0400

    Footer fix

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,6 @@ export const Footer = () => {
   return (
     <div className='footer'>
         <div className='copyright'>&copy; Copyright 2022 | LevTheDev</div>
-        <div className='netlify'><object className="netlifylogo" type="image/svg+xml" data="/netlify-logo.svg">netlify-icon</object>Powered by Netlify </div>
     </div>
   )
 }
